dbfd8b9868c88b999b09d7374515249c5e9c9373
Ganesh Machale <ganeshmachale2006@gmail.com>
2026-09-15 23:42:43 +0530

    Taking String input

--- javaBasics.java
+++ javaBasics.java
@@ -3,10 +3,9 @@
  public class javaBasics{
       public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
-        System.out.println("Enter the value of a & b :");
-         int a = sc.nextInt();
-         int b = sc.nextInt();
-             int sum = a + b ;
-          System.out.println("Sum is:" + sum);
+        System.out.println("Please Enter your full name : ");
+            String Name = sc.nextLine();
+            System.out.println("Your full Name is: " +  Name );
+        
       }
  }
