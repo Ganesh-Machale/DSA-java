962f7eb77f3065d5f31496072b62867f2cbe0787
Ganesh Machale <ganeshmachale2006@gmail.com>
2026-09-15 23:28:28 +0530

    added 2 numbers

--- javaBasics.java
+++ javaBasics.java
@@ -1,5 +1,8 @@
  public class javaBasics{
       public static void main(String[] args) {
-        System.out.print("Hello World!");
+        int a = 10;
+        int b = 12;
+           int c = a + b;
+            System.out.println(c);
       }
  }
