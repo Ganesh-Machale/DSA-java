baf817a074dcbfd956311e90bb90b6a913bf111c
Ganesh Machale <ganeshmachale2006@gmail.com>
2026-09-15 23:38:15 +0530

    implemented addation by input method by using Scanner

--- javaBasics.java
+++ javaBasics.java
@@ -1,8 +1,12 @@
+ import java.util.*;
+
  public class javaBasics{
       public static void main(String[] args) {
-        int a = 10;
-        int b = 12;
-           int c = a + b;
-            System.out.println(c);
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter the value of a & b :");
+         int a = sc.nextInt();
+         int b = sc.nextInt();
+             int sum = a + b ;
+          System.out.println("Sum is:" + sum);
       }
  }
