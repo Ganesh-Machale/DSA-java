4bdf95cc24b3eb7760531e285a04535d5db48559
Ganesh Machale <ganeshmachale2006@gmail.com>
2026-09-16 15:56:10 +0530

    Product of 2 numbers

--- javaBasics.java
+++ javaBasics.java
@@ -3,9 +3,11 @@
  public class javaBasics{
       public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
-        System.out.println("Please Enter your full name : ");
-            String Name = sc.nextLine();
-            System.out.println("Your full Name is: " +  Name );
+        System.out.println("Please Enter the value of a & b : ");
+           int a = sc.nextInt();
+           int b = sc.nextInt();
+           int product =  a * b ;
+           System.out.println("Your product is : " + product );
         
       }
  }
