b16c68e3ccd3deaa9b86569a79d64e2ded52c7c8
Ganesh Machale <ganeshmachale2006@gmail.com>
2026-09-16 16:03:17 +0530

    Area of a circle

--- javaBasics.java
+++ javaBasics.java
@@ -3,11 +3,10 @@
  public class javaBasics{
       public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
-        System.out.println("Please Enter the value of a & b : ");
-           int a = sc.nextInt();
-           int b = sc.nextInt();
-           int product =  a * b ;
-           System.out.println("Your product is : " + product );
-        
+        System.out.println("Please Enter the value radius : ");
+         float rad = sc.nextFloat();
+        float area =  3.14f*rad*rad;
+        System.out.println("Area of a circle is : " + area);
+
       }
  }
