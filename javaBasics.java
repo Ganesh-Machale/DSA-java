f260dbbfc4adbac42a6d3f7ab7d3c69d0b2fbfd8
Ganesh Machale <ganeshmachale2006@gmail.com>
2026-09-17 22:03:14 +0530

    implemented the pre increment

--- javaBasics.java
+++ javaBasics.java
@@ -3,11 +3,9 @@
  public class javaBasics{
       public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
-           char a = 'a';
-           char b = 'b';
-           System.out.println((int)b);
-            System.out.println((int)a);
-           System.out.println(b-a);
-
+           int a = 10;
+           int b = ++a;
+           System.out.println(a);
+           System.out.println(b);
       }
  }
