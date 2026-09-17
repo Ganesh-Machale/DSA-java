b14dae37ed53f2244efb3e814970894639702a4d
Ganesh Machale <ganeshmachale2006@gmail.com>
2026-09-17 22:06:19 +0530

    implemented the post increment

--- javaBasics.java
+++ javaBasics.java
@@ -4,7 +4,7 @@
       public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
            int a = 10;
-           int b = ++a;
+           int b = a++;
            System.out.println(a);
            System.out.println(b);
       }
