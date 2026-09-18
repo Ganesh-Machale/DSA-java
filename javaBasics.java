1a962500a13e184a2f5aae3a8b294f665e398eda
Ganesh Machale <ganeshmachale2006@gmail.com>
2026-09-18 21:36:38 +0530

    learned something

--- javaBasics.java
+++ javaBasics.java
@@ -2,10 +2,9 @@
 
  public class javaBasics{
       public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
+      
            int a = 10;
-           int b = a++;
+            a += 10;
            System.out.println(a);
-           System.out.println(b);
       }
  }
