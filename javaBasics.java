576f2d4aa2676d0f003fcefb0d65df662f1b6d36
Ganesh Machale <ganeshmachale2006@gmail.com>
2026-09-20 21:56:39 +0530

    ternary operator

--- javaBasics.java
+++ javaBasics.java
@@ -3,14 +3,9 @@
  public class javaBasics{
       public static void main(String[] args) {
           Scanner sc = new Scanner (System.in);
-         int A=1 , B=3 , C=6;
-         if ((A>=B)&&(A>=C)){
-            System.out.println("largest is A");
-         }
-         else if(B>=C){
-            System.out.println("Largest is B");
-         }else{
-            System.out.println("Largest is C");
-         }
+         System.out.println("Enter the the Number: ");
+         int Number = sc.nextInt();
+         String Result = ((Number%2) == 0) ? "Even" : "ODD";
+         System.out.println("Answer is :" + Result);
       }
  }
