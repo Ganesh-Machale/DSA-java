940d7df4cc96c4d0f4364e43c19459605536f1c2
Ganesh Machale <ganeshmachale2006@gmail.com>
2026-09-21 11:24:43 +0530

    Implemented Switch case logic in java

--- javaBasics.java
+++ javaBasics.java
@@ -3,9 +3,18 @@
  public class javaBasics{
       public static void main(String[] args) {
           Scanner sc = new Scanner (System.in);
-         System.out.println("Enter the the Number: ");
+         System.out.println("Enter the the Number between 1 to 4 : ");
          int Number = sc.nextInt();
-         String Result = ((Number%2) == 0) ? "Even" : "ODD";
-         System.out.println("Answer is :" + Result);
+         switch(Number){
+             case 1 : System.out.println("I am Coming ");
+             break;
+             case 2 : System.out.println("Geeting package of 60 Lpa at Google");
+               break;
+             case 3 : System.out.println("Meeting with Hitesh Sir");
+             break;
+             case 4 : System.out.println("Going to office ");
+             break;
+             default : System.out.println("Going Home");
+         }
       }
  }
