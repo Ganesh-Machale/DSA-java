3037dd514393181d21b08f37f1567cef09ad29e4
Ganesh Machale <ganeshmachale2006@gmail.com>
2026-09-22 22:08:03 +0530

    printed somthing

--- javaBasics.java
+++ javaBasics.java
@@ -4,7 +4,7 @@
       public static void main(String[] args) {
           Scanner sc = new Scanner (System.in);
           int i =0;
-         while (i<100) {
+         while (i<1000) {
            System.out.println("Googler ");
            i++;
          }
