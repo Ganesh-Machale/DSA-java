a7dd71edcb7d7c46ceaf86b350f63e1d1433c14b
Ganesh Machale <ganeshmachale2006@gmail.com>
2026-09-26 20:57:02 +0530

    Reverse the number but new logic number is storing in another variable

--- javaBasics.java
+++ javaBasics.java
@@ -3,12 +3,14 @@
  public class javaBasics{
       public static void main(String[] args) {
          int n =10899;
-         while(n>0){
-          int lastdigit = n%10;
-          System.out.print(lastdigit);
+         int Rev = 0;
+
+         while(n>0) {
+          int lastdigit = n%10; 
+           Rev = (Rev*10)+lastdigit;
            n = n/10; 
-         }
-         System.out.println();
+         } 
+         System.out.println(Rev);
       }
 
  }
